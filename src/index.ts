779e08029cb5cc4e84002696cdd4df1d21d1115b
0x21106.jsx <aykhanexe@gmail.com>
2025-01-06 21:38:07 +0400

    remove unnecessary initalization codes

--- src/index.ts
+++ src/index.ts
@@ -7,18 +7,3 @@ declare global {
 }
 
 window.CursorEffect = CursorEffect;
-
-const effect = new CursorEffect();
-
-effect.init({
-  toCollide: [
-    ...Array.from(document.querySelectorAll("h1")),
-    ...Array.from(document.querySelectorAll("h2")),
-  ],
-  outerDot: {
-    delay: 30,
-  },
-  innerDot: {
-    delay: 0,
-  },
-});
